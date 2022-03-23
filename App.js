8d13f731c820aca76994d9fa7f8128fc5a2a3eca
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-03-23 10:23:39 +0500

    practice code... intro 1

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View,Text,StyleSheet,Image,TextInput,Button,TouchableOpacity } from 'react-native'
+import { useState } from 'react'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+export default function App(){
+
+  let   [name,setname ] = useState()
+
+  return(
+   <View >
+     <TextInput
+      secureTextEntry={true}
+      keyboardType={"email-address"}
+      maxLength={10}
+      onChangeText={(e)=>setname(e)} //get value and update into variable
+        style={{width:100,margin:20,backgroundColor:"blue",color:"white"}}/>
+     
+     {/* <Button title="onpress" style={{width:200}} onPress={()=>console.log(name)}></Button> */}
+     <View style={{justifyContent:"center",alignItems:"center"}}>
+     <TouchableOpacity style={{backgroundColor:"red"}} activeOpacity={0.5} onPress={()=>console.log(name)}>
+       <Text style={{color:"white",textAlign:"center"} }>onPress</Text>
+
+     </TouchableOpacity>
+     </View>
+ 
+     
+     {/* <View style={styles.header}>
+       <Text>Header</Text>
+     </View>
+     <View style={styles.body}>
+       <Text>Body</Text>
+     </View>
+     <View style={styles.foot}>
+       <Text>Footer</Text>
+     </View>
+     <View style={styles.header}>
+       <Text>Header</Text>
+     </View>
+     <View style={styles.body}>
+       <Text>Body</Text>
+     </View>
+     <View style={styles.foot}>
+       <Text>Footer</Text>
+     </View> */}
+   </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ main:{
+   flex:1,
+   flexDirection:"row",
+   flexWrap:"wrap",
+   justifyContent:"center"
+ },
+ header:{
+   
+   backgroundColor:"red",
+   height:100,
+   width:100
+ },
+ body:{
+ 
+  backgroundColor:"green",
+  height:100,
+  width:100
+ },
+ foot:{
+ 
+  backgroundColor:"yellow",
+  height:100,
+  width:100
+ }
+
+
+})
