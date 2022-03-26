5a8267b19ac4792201e50d53863ae58497533437
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-03-26 11:26:11 +0500

    done

--- App.js
+++ App.js
@@ -1,77 +1,97 @@
-import {View,Text,StyleSheet,Image,TextInput,Button,TouchableOpacity } from 'react-native'
-import { useState } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
 
+export default function App() {
+  let [name, setname] = useState();
 
+  return (
+    <View style={styles.main}>
+      <View style={styles.part1}>
+        <Text>Hello</Text>
+      </View>
+      <View style={styles.part2}>
+        <Text>Hello</Text>
+      </View>
+      <View style={styles.part3}>
 
-export default function App(){
-
-  let   [name,setname ] = useState()
-
-  return(
-   <View >
-     <TextInput
-      secureTextEntry={true}
-      keyboardType={"email-address"}
-      maxLength={10}
-      onChangeText={(e)=>setname(e)} //get value and update into variable
-        style={{width:100,margin:20,backgroundColor:"blue",color:"white"}}/>
-     
-     {/* <Button title="onpress" style={{width:200}} onPress={()=>console.log(name)}></Button> */}
-     <View style={{justifyContent:"center",alignItems:"center"}}>
-     <TouchableOpacity style={{backgroundColor:"red"}} activeOpacity={0.5} onPress={()=>console.log(name)}>
-       <Text style={{color:"white",textAlign:"center"} }>onPress</Text>
-
-     </TouchableOpacity>
-     </View>
- 
-     
-     {/* <View style={styles.header}>
-       <Text>Header</Text>
-     </View>
-     <View style={styles.body}>
-       <Text>Body</Text>
-     </View>
-     <View style={styles.foot}>
-       <Text>Footer</Text>
-     </View>
-     <View style={styles.header}>
-       <Text>Header</Text>
-     </View>
-     <View style={styles.body}>
-       <Text>Body</Text>
-     </View>
-     <View style={styles.foot}>
-       <Text>Footer</Text>
-     </View> */}
-   </View>
-  )
+        <View style={styles.square}>
+        <Text>Hello</Text>
+        </View>
+        <View style={styles.square}>
+        <Text>Hello</Text>
+        </View>
+        <View style={styles.square}>
+        <Text>Hello</Text>
+        </View>
+      </View>
+      <View style={styles.part4}>
+        <Text>Hello</Text>
+      </View>
+      <View style={styles.part5}>
+        <Text>Hello</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
- main:{
-   flex:1,
-   flexDirection:"row",
-   flexWrap:"wrap",
-   justifyContent:"center"
- },
- header:{
-   
-   backgroundColor:"red",
-   height:100,
-   width:100
- },
- body:{
- 
-  backgroundColor:"green",
-  height:100,
-  width:100
- },
- foot:{
- 
-  backgroundColor:"yellow",
-  height:100,
-  width:100
- }
+  main:{
+    flex:1,
+  justifyContent: 'center',
+  }
+  ,
+  part1: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "red",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    aligItems: "center",
+  },
+  part2: {
+    flex: 2,
+    backgroundColor: "yellow",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  part3: {
+    flex: 1,
+    backgroundColor: "grey",
 
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  part4: {
+    flex: 1,
+    backgroundColor: "green",
 
-})
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  part5: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "cyan",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  square:{
+    backgroundColor:'red',
+    marginTop: 50,
+    marginBottom: 50,
+    margin: 10,
+    flex:3
+  }
+  
+});
