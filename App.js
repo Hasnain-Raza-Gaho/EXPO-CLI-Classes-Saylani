8f652d78642e6ad5155ac61c22bb7488411328f1
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-04-02 13:44:58 +0500

    done stack navigation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Navigation from './Src/Config/Navigation';
+import Navigation from './src/config/Navigation';
 
 export default function App() {
   return (
